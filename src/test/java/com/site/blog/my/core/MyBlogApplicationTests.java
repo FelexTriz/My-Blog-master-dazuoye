18221bc5cf327e77a76437943a58054871d9af92
fqj <1229915477@qq.com>
2023-09-18 16:59:51 +0800

    zero modified

--- src/test/java/com/site/blog/my/core/MyBlogApplicationTests.java
+++ src/test/java/com/site/blog/my/core/MyBlogApplicationTests.java
@@ -1,6 +1,7 @@
 package com.site.blog.my.core;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,9 @@ public class MyBlogApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
+
+
 
 }
